28c0719d84b65fc2dcc864aa730e06c4de3ca2e9
purukaushik <purukaushik@asu.edu>
2016-07-06 13:45:15 -0700

    recursive resolution working

--- parse_json.py
+++ parse_json.py
@@ -1,59 +1,64 @@
 import simplejson as json
 from os.path import isfile
 import jsonpath_rw
-from jsonpath_rw import jsonpath
-               
 
-refList = list()
 
-def parse(json_dict):
-    #print "Looking at: " + str(json_dict)
-    #print "Type is: " + str(type(json_dict))
+
+def parse(json_dict, filename):
     # must be one of -> string, dictionary, list
     if isinstance(json_dict,dict):
-        # if it is a dictionary, iterate thru all key,value pairs and add them values whose keys are "$ref";
-        # if key is not "$ref" leave it
+        # if it is a dictionary, iterate thru all key,value pairs
+        
         for key, value in json_dict.items():
+            # if you see a $ref - get the corresponding content from the ref place and return it
             if key == "$ref":
-                refList.append(value)
-                file =  value.split("#")[0]
-                refpath = value.split("#")[1]
-                
-                if isfile(file):
-                    jsondump = json.load(open(file))
+
+                uri_fragments= value.split("#")
+                ref_uri_filename = uri_fragments[0]
+                refpath = uri_fragments[1]
+
+                if not ref_uri_filename.strip():
+                    ref_uri_filename = filename
+                if isfile(ref_uri_filename):
+                    jsondump = json.load(open(ref_uri_filename))
                     refpathexpr = "$" + ".".join(refpath.split("/"))
                     pathexpr = jsonpath_rw.parse(refpathexpr)
                     listofvalues = [match.value for match in pathexpr.find(jsondump)]
 
-                    if len(listofvalues) > 0:
-                        #print json_dict
-                        #print listofvalues[0]
-                        reparse = parse(listofvalues[0])
-                        print reparse
-                        print listofvalues[0]
-                        return listofvalues[0]
-            #print "~"
-            #print str(key),str(value)            
-            
-            ret_val = parse(value)
-            if ret_val is not None:
+                    if len(listofvalues) > 0  :
+                        resolution = listofvalues[0]
+                        recursive_parse = parse(resolution, ref_uri_filename)
+                        print resolution
+                        if recursive_parse is None:
+                            return resolution
+                        else:
+                            print recursive_parse
+                            return resolution
+
+            # just parse the value
+            # and if the return is not None -> replace object. that's a $ref being replaced.
+            resolved = parse(value,filename)
+            if resolved is not None:
                 #print key+ " : " + str(ret_val)
-                json_dict[key] = ret_val
+                json_dict[key] = resolved
     elif isinstance(json_dict,list):
         for (key,value) in enumerate(json_dict):
             #print "#"
-            ret_val = parse(value)
-            if ret_val is not None:
+            resolved = parse(value,filename)
+            if resolved is not None:
                 #print str(key) + ": " + str(ret_val)
-                json_dict[key] = ret_val
+                json_dict[key] = resolved
     return None
 
-#ydicT = json.load(open('main.4.schema.json'))
-dicT = json.load(open('test_schema.json'))
+filename  = 'main.4.schema.json'
+#filename  = 'test_schema.json'
+#filename = 'ref_schema.json'
+dicT = json.load(open(filename))
+#dicT = json.load(open('test_schema.json'))
 
-parse(dicT)
+parse(dicT,filename)
 new_file = open('test_schema_resolved.json', 'w')
-#json.dump(dicT,new_file)
+json.dump(dicT,new_file)
 
 #import json, os
 #from jsonschema import validate, ValidationError
