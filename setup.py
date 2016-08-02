ecd36e882665d7a5649adca357d3a309d9542a08
purukaushik <purukaushik@asu.edu>
2016-08-02 08:41:14 -0700

    Make it a installable package

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ref-resolver',
+      version='1.0',
+      description='A lightweight python json schema ref resolver and inliner.',
+      author='Purush Swaminathan',
+      author_email='purukaushik@asu.edu',
+      url='https://github.com/purukaushik/ref-resolver',
+      download_url='https://github.com/purukaushik/ref-resolver.git',
+      packages=['ref_resolver'],
+     )
