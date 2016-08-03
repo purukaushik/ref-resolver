8d6d4b037ffddeb6caa40f60ae1825f2f4d070ce
Purush Swaminathan <purukaushik@asu.edu>
2016-08-03 16:17:51 -0700

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='ref-resolver',
-      version='1.0.1',
+      version='1.0.2',
       description='A lightweight python json schema ref resolver and inliner.',
       author='Purush Swaminathan',
       author_email='purukaushik@asu.edu',
